7ac3a6139f8ae1b6603527f09b4034f262576dea
WilliamBaltus <wbaltus21@gmail.com>
2022-04-25 11:57:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,23 +51,75 @@ public:
     }
 };
 
-
+Person add(){
+    cout << endl;
+    cout << setfill(' ') << setw(59) << "1. Add Teacher" << endl;
+    cout << setfill(' ') << setw(59) << "2. Add Student" << endl;
+    string name;
+    int age;
+    char gender;
+    int input = 0;
+    cin >> input;
+    switch (input) {
+        case 1: {
+            cout << "Add teacher selected" << endl;
+            cout << "Provide a name for this teacher: ";
+            cin >> name;
+            cout << "Provide an age for " << name << ": ";
+            cin >> age;
+            cout << "Provide a gender (M/F) for " << name << ": ";
+            cin >> gender;
+            Person temp (name, age, gender);
+            return temp;
+        }
+        case 2:{
+            cout << "Add student selected " << endl;
+            break;
+        }
+        default:
+            break;
+    }
+}
 int main() {
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    //cout << setfill(' ') << setw(70)<< "Welcome to the Teacher-Student GUI " << endl;
-    cout << endl;
-    cout << setfill('_') <<setw(120)<< "_" << endl;
-    cout << setfill(' ') << setw(60)<< "Press a key " << endl;
-    cout << setfill(' ') << setw(60)<< "1. Exit     " << endl;
-    cout << setfill(' ') << setw(60)<< "2. View     " << endl;
-    cout << setfill(' ') << setw(60)<< "3. Add      " << endl;
-    cout << setfill(' ') << setw(60)<< "4. Update   " << endl;
-    cout << setfill(' ') << setw(60)<< "5. Remove   " << endl;
-    cout << setfill('_') << setw(120)<< "_" << endl;
-    Person("Billy", 21, 'M');
-    return 0;
+    while(1) {
+        cout << endl;
+        cout << endl;
+        cout << endl;
+        cout << endl;
+        cout << endl;
+        cout << setfill(' ') << setw(70)<< "Welcome to the Teacher-Student GUI " << endl;
+        cout << endl;
+        cout << setfill('_') << setw(120) << "_" << endl;
+        cout << setfill(' ') << setw(60) << "Press a key " << endl;
+        cout << setfill(' ') << setw(60) << "1. Exit     " << endl;
+        cout << setfill(' ') << setw(60) << "2. View     " << endl;
+        cout << setfill(' ') << setw(60) << "3. Add      " << endl;
+        cout << setfill(' ') << setw(60) << "4. Update   " << endl;
+        cout << setfill(' ') << setw(60) << "5. Remove   " << endl;
+        cout << setfill('_') << setw(120) << "_" << endl;
+        int input = 0;
+        cin >> input;
+        switch (input) {
+            case 1:
+                cout << "EXITING" << endl;
+                exit(1);
+            case 2:
+                cout << "View selected " << endl;
+                break;
+            case 3: {
+                cout << "Add selected " << endl;
+                Person init = add();
+                break;
+            }
+            case 4:
+                cout << "Update selected " << endl;
+                break;
+            case 5:
+                cout << "Remove selected " << endl;
+                break;
+            default:
+                cout << "INVALID KEY " << endl;
+                break;
+        }
+    }
 }
